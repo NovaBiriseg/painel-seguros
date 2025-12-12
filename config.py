46a2ea56cf3e94406fde2757ebb1841eabbe778a
NovaBiriseg <sinistros.novabiriseg@gmail.com>
2025-12-12 08:58:03 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,18 @@
-# config.py - URL export do Google Sheets (XLSX)
-EXCEL_URL = "https://docs.google.com/spreadsheets/d/18DKFhsyTjJZcG7FqfB757DkPDDA2YgT0/export?format=xlsx"
+import pandas as pd
+import streamlit as st
+
+def load_sheet():
+    """
+    Carrega a planilha do Google Sheets usando a URL definida em st.secrets.
+    """
+    try:
+        sheet_url = st.secrets["sheet_url"]
+    except KeyError:
+        raise Exception("Nenhuma URL configurada em st.secrets")
+
+    if not sheet_url.startswith("https://docs.google.com/spreadsheets"):
+        raise Exception("URL inválida de Google Sheets")
+
+    csv_url = sheet_url.replace("/edit#gid=", "/export?format=csv&gid=")
+
+    return pd.read_csv(csv_url)
